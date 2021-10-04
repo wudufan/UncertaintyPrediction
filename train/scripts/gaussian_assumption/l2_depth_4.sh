7db2772c63d58807fb09351594c7d3f8231143fc
Dufan Wu <dwu6@mgh.harvard.edu>
2021-10-03 20:07:22 -0400

    update config

--- train/scripts/gaussian_assumption/l2_depth_4.sh
+++ train/scripts/gaussian_assumption/l2_depth_4.sh
@@ -8,9 +8,9 @@
 
 cd ../../
 
-python3 train_gaussian_assumption.py "./config/gaussian_assumption/l2_depth_4.cfg" --Train.device "'2'" \
+python3 train_gaussian_assumption.py "./config/gaussian_assumption/l2_depth_4.cfg" --Train.device "'1'" \
 --IO.tag "'l2_depth_4/img'" \
 --Network.input_shape "(640, 640, 1)" \
 --Data.num_patches_per_slice "1" --Data.num_slices_per_batch "1" \
 --Train.epochs "[20, 50, 100]" --Train.lr "[3.3e-5, 1e-5, 1e-5]" \
-&> ./outputs/gaussian_assumption/l2_depth_4/img.log
+&> ./outputs/gaussian_assumption/l2_depth_4_img.log
--- train/scripts/uncertainty/l2_depth_4.sh
+++ train/scripts/uncertainty/l2_depth_4.sh
@@ -13,12 +13,12 @@ python3 train_uncertainty.py "./config/uncertainty/l2_depth_4.cfg" --Train.devic
 --Network.input_shape "(640, 640, 2)" \
 --Data.num_patches_per_slice "1" --Data.num_slices_per_batch "1" \
 --Train.epochs "[20, 50, 100]" --Train.lr "[1e-4, 3.3e-5, 1e-5]" \
-&> ./outputs/uncertainty/l2_depth_4/img.log &
+&> ./outputs/uncertainty/l2_depth_4_img.log &
 
 python3 train_uncertainty.py "./config/uncertainty/l2_depth_4.cfg" --Train.device "'3'" \
 --IO.tag "'l2_depth_4/patch'" \
 --Network.input_shape "(64, 64, 2)" --Data.patch_size "(64, 64, 2)" \
 --Data.num_patches_per_slice "25" --Data.num_slices_per_batch "4" \
 --Train.epochs "[80, 200, 400]" --Train.lr "[1e-4, 3.3e-5, 1e-5]" --Train.save_freq "20" \
-&> ./outputs/uncertainty/l2_depth_4/patch.log &
+&> ./outputs/uncertainty/l2_depth_4_patch.log &
 wait
